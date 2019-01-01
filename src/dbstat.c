a2127af2a806298e22bb1ebb28112d57b7495d8c
Michal Kukowski <michalkukowski10@gmail.com>
2019-01-01 11:46:06 +0100

    Fix db stat

--- src/dbstat.c
+++ src/dbstat.c
@@ -74,9 +74,12 @@ void db_stat_finish_querry(void)
     }
     else /* now min = min(min, current) */
     {
-        db_minimum_snapshot.read_time = MIN(db_minimum_snapshot.read_time, db_current_querry.read_time);
-        db_minimum_snapshot.write_time = MIN(db_minimum_snapshot.write_time, db_current_querry.write_time);
-        db_minimum_snapshot.erase_time = MIN(db_minimum_snapshot.erase_time, db_current_querry.erase_time);
+        if (db_stat_get_min_time() > db_stat_get_current_time())
+        {
+            db_minimum_snapshot.read_time = db_current_querry.read_time;
+            db_minimum_snapshot.write_time = db_current_querry.write_time;
+            db_minimum_snapshot.erase_time = db_current_querry.erase_time;
+        }
         db_minimum_snapshot.memory_usage = MIN(db_minimum_snapshot.memory_usage, db_current_querry.memory_usage);
     }
 
@@ -89,9 +92,12 @@ void db_stat_finish_querry(void)
     db_total.memory_usage = db_current_querry.memory_usage;
 
     /* update maximum snapshot */
-    db_maximum_snapshot.read_time = MAX(db_maximum_snapshot.read_time, db_current_querry.read_time);
-    db_maximum_snapshot.write_time = MAX(db_maximum_snapshot.write_time, db_current_querry.write_time);
-    db_maximum_snapshot.erase_time = MAX(db_maximum_snapshot.erase_time, db_current_querry.erase_time);
+    if (db_stat_get_max_time() < db_stat_get_current_time())
+    {
+        db_maximum_snapshot.read_time = db_current_querry.read_time;
+        db_maximum_snapshot.write_time = db_current_querry.write_time;
+        db_maximum_snapshot.erase_time = db_current_querry.erase_time;
+    }
     db_maximum_snapshot.memory_usage = MAX(db_maximum_snapshot.memory_usage, db_current_querry.memory_usage);
 }
 
